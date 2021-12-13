66eafadf572d311b6fd92c270e144534ada72a25
Yeison <enmanuelquiterios@gmail.com>
2021-12-12 23:09:50 -0500

    Added methods to my applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+
+    private 
+
+
+    def current_user 
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+
+    def authenticate_user 
+        return render json: {error: 'Not authorized'}, status: :unauthorized unless current_user
+    end
 end
